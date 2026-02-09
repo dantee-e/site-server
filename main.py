e611574ecd56e39ebcb00232ee8fb8838db079c0
Dante Ramacciotti <dante.ramacciotti@uni-jena.de>
2026-02-09 23:33:57 +0100

    Made reverse proxy
    Changed how the website works. Instead of redirecting to the service, it
    creates a reverse proxy to it

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
+import httpx
+from httpx import AsyncClient
 from fastapi import FastAPI, Request
-from fastapi.responses import RedirectResponse, HTMLResponse
+from fastapi.responses import HTMLResponse, StreamingResponse
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
+from starlette.background import BackgroundTask
 
 app = FastAPI()
 
@@ -15,9 +18,45 @@ def main(request: Request):
     return templates.TemplateResponse("select_menu.html", {"request": request})
 
 
-@app.api_route("/jellyfin")
-async def proxy(_: Request):
-    # Target server on another port
-    target_url = "http://localhost:8096"
+async def _reverse_proxy(
+    request: Request, server: AsyncClient, path, prefix
+) -> StreamingResponse:
+    url = httpx.URL(path=path, query=request.url.query.encode("utf-8"))
+    rp_req = server.build_request(
+        request.method, url, headers=request.headers.raw, content=await request.body()
+    )
+    rp_resp = await server.send(rp_req, stream=True)
+    headers = dict(rp_resp.headers)
+    if "location" in headers:
+        location = headers["location"]
+        # If it's an absolute URL pointing to the backend
+        if location.startswith(str(server.base_url)):
+            # Replace backend URL with proxy URL
+            location = location.replace(str(server.base_url).rstrip("/"), prefix)
+            print(f"location is {location}")
+            headers["location"] = location
+        # If it's a relative URL, prepend the prefix
+        else:  # location.startswith("/"):
+            headers["location"] = f"{prefix}{location}"
+            print(f"location is {location}")
 
-    return RedirectResponse(target_url)
+    return StreamingResponse(
+        rp_resp.aiter_raw(),
+        status_code=rp_resp.status_code,
+        headers=headers,
+        background=BackgroundTask(rp_resp.aclose),
+    )
+
+
+JELLYFIN = AsyncClient(base_url="http://localhost:8096/")
+
+
+# For each service, add an api_route like the one underneath  and call
+#  the _reverse_proxy function passing as a parameter the path and the
+#  AsyncClient that's connected to the service port
+@app.api_route(
+    "/jellyfin{path:path}",
+    methods=["GET", "POST", "PUT", "DELETE", "PATCH", "HEAD", "OPTIONS"],
+)
+async def proxy(request: Request, path: str):
+    return await _reverse_proxy(request, JELLYFIN, path, "/jellyfin/")
